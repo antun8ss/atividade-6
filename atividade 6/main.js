9bb89540c72122f9a486bf3f0a2de6cf4a099d88
Gabriel Rocha <gabriel.rocha.antunes@escola.pr.gov.br>
2025-08-29 10:00:44 -0300

    Add files via upload

---
+++ atividade 6/main.js
@@ -0,0 +1,17 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#outPromocao");
+const resp2 = document.querySelector("#outPrecoTerceiro");
+
+
+frm.addEventListener("submit", (e) =>{
+    const produto = frm.inProduto.value;
+    const preco = Number(frm.inPreco.value);
+
+    const teceiro = preco / 2;
+    const total = (preco * 2) + terceiro;
+
+    resp1.innerText = `${produto} - Promoção: Leve 3 por: R$ ${total.toFixed(2)}`;
+    resp2.innerText = `O 3 produto custa apenas: R$ ${terceiro.toFixed(2)}`;
+    
+    e.preventDefault();
+})
